710324544c8b7bc961340621139110b05b721988
Matej - BlackCircle <mail@matej.us>
2020-06-08 01:57:16 +0200

    initial

---
+++ app/application.h
@@ -0,0 +1,17 @@
+#ifndef _APPLICATION_H
+#define _APPLICATION_H
+
+#ifndef VERSION
+#define VERSION "0.1"
+#endif
+
+#ifndef APP_NAME
+#define APP_NAME "W-meter"
+#endif
+
+
+#include <bcl.h>
+#include <bc_module_lcd.h>
+
+
+#endif
